0d66216c1e50f46c5833322c061f26e2faee756e
Rushad Nabiullin <rushad.nab@gmail.com>
2017-11-19 16:01:54 +0400

    handle empty cookies

--- middlewares/cookie-parser.js
+++ middlewares/cookie-parser.js
@@ -1,5 +1,5 @@
 function cookieParser(req, res, next) {
-    const cookies = req.headers.cookie.split(';').map(cookie => cookie.trim());
+    const cookies = (req.headers.cookie || '').split(';').map(cookie => cookie.trim());
     req.parsedCookies = cookies.reduce((parsedCookies, cookie) => {
         const parsedCookie = cookie.split('=');
         parsedCookies[parsedCookie[0]] = parsedCookie[1];
